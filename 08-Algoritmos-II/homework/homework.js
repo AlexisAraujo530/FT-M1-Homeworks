366ad04170dc81f6d22e715ba009dd330c6f8be8
AlexisAraujo530 <alexisaraujo530@gmail.com>
2022-09-21 14:00:14 -0300

    Homework-8

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,14 +6,48 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  
+  if(array.length < 1) {
+    return [];
+  }
+  let pivote = array[0];
+  let izq = [];
+  let der = [];
+  
+  for(let i = 1; i < array.length; i++) {
+    if(array[i] < pivote) {
+      izq.push(array[i]);
+    } else {
+      der.push(array[i]);
+    }
+  }
+  return [].concat(quickSort(izq), pivote, quickSort(der));
+
 
 }
-
+// [5, 1, 4, 2, 8]
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  //dividir el array
+  //condicion de corte --> un solo elemento
+  if(array.length < 1) {
+    return [];
+  }
+  let pivote = array[0];
+  let izq = [];
+  let der = [];
+  for(let i = 1; i < array.length; i++) {
+    if(array[i] < pivote) {
+      izq.push(array[i]);
+    } else {
+      der.push(array[i]);
+    }
+  }
+  return [].concat(mergeSort(izq), pivote, mergeSort(der));
+  
 
 }
 
